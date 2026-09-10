63adc6f966800c88a7f75984cc97b2d407a2a846
priya-shetye <priya_shetye@hotmail.com>
2026-09-10 18:37:03 +0530

    updated config file

--- training-docs/docusaurus.config.js
+++ training-docs/docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Priya doc site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://priya-shetye.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'priya-shetye', // Usually your GitHub org/user name.
+  projectName: 'training-v1', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
